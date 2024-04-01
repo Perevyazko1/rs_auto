fbfa62bffa8af6ba67c31e0336a64c8101b772d6
Perevyazko <a.perevyazko@gmail.com>
2024-04-02 00:06:52 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const tg = (window as any).Telegram.WebApp
+  const [init, setInit] = useState(tg?.initData)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width: "100px", wordWrap: "break-word" }}>
+      {init}
     </div>
   );
 }
